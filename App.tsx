f77e20f6335ef7120a18657740701b776fc35a24
SouravM16 <souravmalviya08@gmail.com>
2025-10-07 01:27:10 +0530

    change in footer

--- App.tsx
+++ App.tsx
@@ -70,7 +70,7 @@ const App: React.FC = () => {
         </div>
         <footer className="text-center mt-8">
           <p className="text-sm text-slate-500 dark:text-slate-400">
-             Built by Sourav Malviya
+            By Sourav Malviya 
           </p>
         </footer>
       </main>
